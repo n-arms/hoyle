5815af21717ea00fdd329336b933b80a858a019e
n-arms <75282394+n-arms@users.noreply.github.com>
2024-07-29 14:59:20 -0400

    Implement compilation of generic functions

--- emit/src/emitter.rs
+++ emit/src/emitter.rs
@@ -88,6 +88,16 @@ typedef struct _witness {
   void (*move)(void *, void *, void *);
   void *extra;
 } _witness;
+
+void move_F64(void *dest, void *src, void *extra) {
+  memmove(dest, src, 8);
+}
+
+void F64(void *_result) {
+  _witness *result = _result;
+  result -> move = move_F64;
+  result -> extra = NULL;
+}
 "#,
     );
     for (to_emit, env) in program.functions.into_iter().zip(envs) {
